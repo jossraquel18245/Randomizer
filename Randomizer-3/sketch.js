08dab458360d8215fa90c7b232cb7312f55bf916
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-10-06 04:02:45 -0700

    Added Photos Randomizer-3

--- Randomizer-3/sketch.js
+++ Randomizer-3/sketch.js
@@ -1,33 +1,62 @@
+let flowers =
 
-let lolChampions = [{
+[{
 
-  name: "Ashe",
-  ability: "Arrow"
-},{
-  name: "Garen",
-  ability: "Sword"
+  name: "Rose",
+  personality: "you have a quiet and traditional exterior, but inside, you are passionate, romantic and deep"
+}, {
+  name: "CherryBlossom",
+  personality: "You are intelligent, honest and sweet. You are friendly to everybody and don't like conflict."
 
-},{
-  name: "Teemo",
-  ability: "Poison"
+}, {
+  name: "Sunflower",
+  personality: "You're optimistic and cheerful. You create a place of love, happiness, and warmth."
 
-},{
-  name: "Volibear",
-  ability: "Lightning"
+}, {
+  name: "BirdsOfParadise",
+  personality: "you are attractive, energetic and highly dramatic"
 
-},{
-  name: "Annie",
-  ability: "Tibers"
+}, {
+  name: "Daisy",
+  personality: "you are positive, morning person, full of life, & a loyal person. "
+
+}, {
+  name: "LotusFlower",
+  personality: "you have a pure heart, you are quiet and reserved, you pick your friends carefully"
+
+}, {
+  name: "Lily",
+  personality: "you are a family person, good provider, and protector"
+
+}, {
+  name: "Peony",
+  personality: "you are sweet and appreciate small gestures."
 
 }];
 
 let randomIndex;
 let animating = false;
+let imagesofflowers = [];
+let imageCounter = 0;
+let margin = 20;
+
+function preload() {
+
+  for (let i = 0; i < flowers.length; i++) {
+    imagesofflowers[i] = loadImage("assets/" + flowers[i].name + ".jpg")
+  }
+}
 
 function setup() {
-  createCanvas(400, 400);
-  textSize(32);
-  text ("Click to Randomize", 50,50);
+  createCanvas(800, 800);
+  textSize(24);
+  imageMode(CENTER);
+  frameRate(4);
+
+
+  //Set up the background and text for Click to get a Flower
+  background(random(125, 136), random(205, 214), random(220, 250));
+  text("Click to Get a Flower", 250, 50);
 
 
 
@@ -35,33 +64,35 @@ function setup() {
 
 function draw() {
 
-  if(animating==true){
-    ellipse(random(width), random(height), random(50,100));
-    }
-
-}
-
-function randomizer(){
-  animating = false;
-  background(random (200,255),0,0);
-  if (lolChampions.length >0){
-
-    randomIndex= int(random(lolChampions.length));
-    text(`${lolChampions[randomIndex].name} 's ability
-      ${lolChampions[randomIndex].ability}`,50,50);
-
-    lolChampions.splice(randomIndex, 1);
-  }
-  else {
-    text("Nothing Left!", 50, 50)
+  if (animating == true) {
+    // Randomly cycle through available images
+    background(random(152), random(188, 255), random(127, 152));
+    index = int(random(imagesofflowers.length));
+    image(imagesofflowers[index], 400, 400);
   }
 }
 
 
+function mousePressed() {
+  if (flowers.length > 0) {
 
-function mousePressed(){
-  animating = true;
-  setTimeout(randomizer,2000);
+    animating = true;
+    setTimeout(randomizer, 2000);
+  } else {
+    background(random(200, 250));
+    text("You have no more flowers!") (250, 50);
+  }
 
+  function randomizer(){
+    animating = false;
+    background(random(130, 180), random(200, 225), random(240, 250));
+    if (flowers.length > 0) {
+      randomIndex = int(random(flowers.length));
+      image(imagesofflowers[randomIndex], 400, 400);
+      descrption = flowers[randomIndex].name + " uses a\n" + flowers[randomIndex].personality + "!";
 
+      // Remove the champion and its image from their corresponding lists
+    flowers.splice(randomIndex, 1);
+    imagesofflowers.splice(randomIndex, 1);
+}
 }
